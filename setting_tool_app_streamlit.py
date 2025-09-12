23414b34155ff42c54887a84e7eca95fa624ac46
negitama068-cyber <negitama068@gmail.com>
2025-09-13 00:11:34 +0900

    Update setting_tool_app_streamlit.py

--- setting_tool_app_streamlit.py
+++ setting_tool_app_streamlit.py
@@ -68,9 +68,9 @@ st.title("防振り 設定判別ツール")
 st.header("初あたり関連")
 col1, col2, col3 = st.columns(3)
 with col1:
-    atari_total = st.number_input("初あたり回数", 0, 1000, 10)
+    atari_total = st.number_input("初あたり回数", 0, 1000, 3)
 with col2:
-    bet_hit = st.number_input("BET高確発生回数", 0, 1000, 0)
+    bet_hit = st.number_input("BET高確発生回数", 0, 1000, 1)
 with col3:
     direct_hit = st.number_input("直撃回数", 0, 1000, 0)
 
@@ -84,28 +84,28 @@ with col2:
 st.header("ゲーム経由CZ")
 col1, col2 = st.columns(2)
 with col1:
-    total300 = st.number_input("300ゲーム経由回数", 0, 1000, 5)
+    total300 = st.number_input("300ゲーム経由回数", 0, 1000, 3)
 with col2:
-    hit300 = st.number_input("300ゲームCZ当選回数", 0, 1000, 2)
+    hit300 = st.number_input("300ゲームCZ当選回数", 0, 1000, 1)
 
 col3, col4 = st.columns(2)
 with col3:
-    total450 = st.number_input("450ゲーム経由回数", 0, 1000, 4)
+    total450 = st.number_input("450ゲーム経由回数", 0, 1000, 2)
 with col4:
-    hit450 = st.number_input("450ゲームCZ当選回数", 0, 1000, 1)
+    hit450 = st.number_input("450ゲームCZ当選回数", 0, 1000, 0)
 
 col5, col6 = st.columns(2)
 with col5:
-    total650 = st.number_input("650ゲーム経由回数", 0, 1000, 3)
+    total650 = st.number_input("650ゲーム経由回数", 0, 1000, 0)
 with col6:
-    hit650 = st.number_input("650ゲームCZ当選回数", 0, 1000, 1)
+    hit650 = st.number_input("650ゲームCZ当選回数", 0, 1000, 0)
 
 st.header("ボナ終了時コメント")
 col1, col2 = st.columns(2)
 with col1:
-    sally = st.number_input("サリーしか勝たん出現回数", 0, 1000, 2)
+    sally = st.number_input("サリーしか勝たん出現回数", 0, 1000, 5)
 with col2:
-    maple = st.number_input("メイプルしか勝たん出現回数", 0, 1000, 1)
+    maple = st.number_input("メイプルしか勝たん出現回数", 0, 1000, 5)
 comment_data = {'sally': sally, 'maple': maple}
 
 observed = {
@@ -128,6 +128,7 @@ if st.button("設定を推定する"):
 st.info("""
 - このツールの推定結果はあくまで参考値ですので、参考程度にお願いします。
 """)
+
 
 
 
